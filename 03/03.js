9f9bd80b2a7158bb2a7aa6e9458753e6032309e5
pania007 <serejenka22@gmail.com>
2020-01-03 19:15:35 +0200

    Serj commit

---
+++ 03/03.js
@@ -0,0 +1,17 @@
+var q = 152;
+var w = 52;
+var e = 600;
+if ( q < w){
+    if (q < e){
+        console.log ( " минимальная переменная q = " + q);
+    }
+}
+if ( w < q){
+    if (w < e){
+        console.log (" минимальная переменная w = " + w);
+    }
+}
+if ( e < q){
+    if (e < w){
+        console.log (" минимальная переменная e = " + e);
+    }
